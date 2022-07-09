ce97713d75dfc06fff4fff716ac68b156ee0d005
ikuma <97657938+ikkundayo@users.noreply.github.com>
2022-07-09 23:03:47 +0900

    save

---
+++ src/test.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class test {
+  public static void main(String[] args) throws Exception {
+    Scanner scan = new Scanner(System.in);
+    // int text = scan.nextInt();
+    String text = scan.next();
+
+    System.out.println(text);
+    if (text.equals("Java")) {
+      System.out.println("Welcome");
+  }
+
+    // if (text == 10) {
+    //   System.out.println("welcome");
+    // } else if (text > 10) {
+    //   System.out.println("hello");
+    // } else {
+    //   System.out.println("bey");
+    // }
+
+    // int i = 0;
+    // while (i < 10) {
+    //   System.out.println(i + 1);
+    //   i++;
+    // }
+
+    // int[] list = {1, 2, 3};
+    //   // for (int i = 0; i < list.length; i++) {
+    //   //   System.out.println(list[i]);
+    //   // }
+    //   for (int a : list) {
+    //     System.out.println(a);
+    // }
+  }
+}
